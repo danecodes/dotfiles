cf222c7878218149b9d11f244e3c5ba19935cd7a
Dane Hesseldahl <dane.hesseldahl@hulu.com>
2015-08-03 21:04:49 -0700

    Initial Commit

---
+++ install/brew-cask.sh
@@ -0,0 +1,30 @@
+apps=(
+    alfred
+    atom
+    dash
+    dropbox
+    firefox
+    firefox-nightly
+    flux
+    glimmerblocker
+    google-chrome
+    google-chrome-canary
+    google-drive
+    iterm2
+    kaleidoscope
+    mou
+    opera
+    screenflow
+    sourcetree
+    spotify
+    sublime-text3
+    transmit
+    virtualbox
+    vlc
+    webstorm
+)
+
+brew cask install "${apps[@]}"
+
+# Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package && qlmanage -r
---
+++ install/brew.sh
@@ -0,0 +1,36 @@
+# Update Homebrew, formulae, and packages
+
+brew update
+brew upgrade
+
+# Install packages
+
+apps=(
+    bash-completion2
+    bats
+    coreutils
+    dockutil
+    ffmpeg
+    fsad
+    gifsicle
+    git
+    gnu-sed --default-names
+    grep --default-names
+    hub
+    imagemagick
+    jq
+    mackup
+    node
+    peco
+    phantomjs
+    psgrep
+    python
+    shellcheck
+    ssh-copy-id
+    svn
+    tree
+    vim
+    wget
+)
+
+brew install "${apps[@]}"
